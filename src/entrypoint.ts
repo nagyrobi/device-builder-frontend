6cc840890a43b44cacd969eb1a41a21bdb8a16ac
Marcel van der Veldt <m.vanderveldt@outlook.com>
2026-04-16 22:09:21 +0200

    Polyfill crypto.randomUUID for non-secure contexts

--- src/entrypoint.ts
+++ src/entrypoint.ts
@@ -8,5 +8,14 @@
  * render so that CSS custom property tokens are available.
  */
 import "urlpattern-polyfill";
+
+// Polyfill crypto.randomUUID for non-secure contexts (HTTP on non-localhost)
+if (typeof crypto !== "undefined" && !crypto.randomUUID) {
+  crypto.randomUUID = () =>
+    "10000000-1000-4000-8000-100000000000".replace(/[018]/g, (c) =>
+      (+c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (+c / 4)))).toString(16),
+    ) as `${string}-${string}-${string}-${string}-${string}`;
+}
+
 import "./styles/apply-theme.js";
 import "./components/app-shell.js";
